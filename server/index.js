ef5db3b30a43c85fb6814e28e0b69a2311024db1
HutaJoullach <hutajoullach@gmail.com>
2023-01-22 15:51:30 +0900

    added auth route

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const authRouter = require("./routes/auth");
 
@@ -9,6 +10,16 @@ const DB = "";
 // Middleware
 app.use(express.json());
 app.use(authRouter);
+
+// Connection
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(PORT, () => {
   console.log(`connected at port ${PORT}`);
