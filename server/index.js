a237a869af06b971f13b9bd929e47b82886f3fe8
HutaJoullach <hutajoullach@gmail.com>
2023-01-24 22:26:20 +0900

    added sign in route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const authRouter = require("./routes/auth");
 
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 const app = express();
-const DB = "";
+const DB = process.env.CONNECTION_URL;
 
 // Middleware
 app.use(express.json());
