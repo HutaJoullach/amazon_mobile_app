4342c73229be8b8d8f8c68cffe9e3583a643b631
HutaJoullach <hutajoullach@gmail.com>
2023-01-28 23:48:30 +0900

    added auth service, user model

---
+++ lib/features /auth/services/auth_service.dart
@@ -0,0 +1,41 @@
+class AuthService {
+  // sign up user
+  void signUpUser({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      User user = User(
+        id: '',
+        name: name,
+        password: password,
+        email: email,
+        address: '',
+        type: '',
+        token: '',
+        cart: [],
+      );
+
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signup'),
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackBar(
+            context,
+            'Account created! Login with the same credentials!',
+          );
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
