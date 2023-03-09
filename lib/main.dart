8eac9d0a97eb3a08dc3d42eb2f4e2054ab2f5bd1
HutaJoullach <hutajoullach@gmail.com>
2023-03-09 10:27:43 +0900

    added home_screen, user_provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,36 @@
-// import 'dart:html';
-
+import 'package:amazon_mobile_app/common/widgets/bottom_bar.dart';
 import 'package:amazon_mobile_app/constants/global_variables.dart';
-import 'package:amazon_mobile_app/features%20/auth/screens/auth_screen.dart';
+import 'package:amazon_mobile_app/features/admin/screens/admin_screen.dart';
+import 'package:amazon_mobile_app/features/auth/screens/auth_screen.dart';
+import 'package:amazon_mobile_app/features/auth/services/auth_service.dart';
+import 'package:amazon_mobile_app/providers/user_provider.dart';
 import 'package:amazon_mobile_app/router.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+    ),
+  ], child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    authService.getUserData(context);
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,14 @@
-import 'package:amazon_mobile_app/features%20/auth/screens/auth_screen.dart';
+import 'package:amazon_mobile_app/common/widgets/bottom_bar.dart';
+import 'package:amazon_mobile_app/features/address/screens/address_screen.dart';
+import 'package:amazon_mobile_app/features/admin/screens/add_product_screen.dart';
+import 'package:amazon_mobile_app/features/auth/screens/auth_screen.dart';
+import 'package:amazon_mobile_app/features/home/screens/category_deals_screen.dart';
+import 'package:amazon_mobile_app/features/home/screens/home_screen.dart';
+import 'package:amazon_mobile_app/features/order_details/screens/order_details.dart';
+import 'package:amazon_mobile_app/features/product_details/screens/product_details_screen.dart';
+import 'package:amazon_mobile_app/features/search/screens/search_screen.dart';
+import 'package:amazon_mobile_app/models/order.dart';
+import 'package:amazon_mobile_app/models/product.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
@@ -7,6 +17,11 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
       return MaterialPageRoute(
         settings: routeSettings,
         builder: (_) => const AuthScreen(),
+      );
+    case HomeScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const HomeScreen(),
       );
     default:
       return MaterialPageRoute(
